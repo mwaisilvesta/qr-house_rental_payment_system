93f53835d7ac80e7094f05dcf19865dbbcbc8c91
mwaisilvesta <mwaisilvester74@gmail.com>
2024-04-03 01:15:02 +0300

    adding background image

--- maintainance_request.php
+++ maintainance_request.php
@@ -31,6 +31,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         body {
             font-family: Arial, sans-serif;
             background-color: #f4f4f4;
+            background-image: url('rent.png'); /* Add the path to your background image */
+            background-size: cover; /* Cover the entire background */
+            background-position: center; /* Center the background image */
             margin: 0;
             padding: 0;
         }
