d9b6b61ac42d84ab1271543c4809830067b047e0
mwaisilvesta <mwaisilvester74@gmail.com>
2024-04-05 09:10:53 +0300

    adding  composer file

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'fbc4d3fa082a9afcb0c7029cf5149b34cbb95cc0',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'fbc4d3fa082a9afcb0c7029cf5149b34cbb95cc0',
+            'dev_requirement' => false,
+        ),
+        'stripe/stripe-php' => array(
+            'pretty_version' => 'v13.17.0',
+            'version' => '13.17.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../stripe/stripe-php',
+            'aliases' => array(),
+            'reference' => '24d3d9ed8406e10d24452af44597300173655f69',
+            'dev_requirement' => false,
+        ),
+    ),
+);
