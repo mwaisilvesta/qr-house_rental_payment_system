09ad9f6f36719002d1771e337e5b5a05a7c57152
mwaisilvesta <mwaisilvester74@gmail.com>
2024-04-08 07:43:17 +0300

    updating db schema

--- database/house_rental_db.sql
+++ database/house_rental_db.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Oct 26, 2020 at 07:07 AM
--- Server version: 10.4.14-MariaDB
--- PHP Version: 7.2.33
+-- Host: localhost
+-- Generation Time: Apr 08, 2024 at 06:34 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -30,7 +30,7 @@ SET time_zone = "+00:00";
 CREATE TABLE `categories` (
   `id` int(30) NOT NULL,
   `name` varchar(200) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `categories`
@@ -40,7 +40,8 @@ INSERT INTO `categories` (`id`, `name`) VALUES
 (1, 'Duplex'),
 (2, 'Single-Family Home'),
 (3, 'Multi-Family Home'),
-(4, '2-story house');
+(4, '2-story house'),
+(5, 'shops');
 
 -- --------------------------------------------------------
 
@@ -54,14 +55,68 @@ CREATE TABLE `houses` (
   `category_id` int(30) NOT NULL,
   `description` text NOT NULL,
   `price` double NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `houses`
 --
 
 INSERT INTO `houses` (`id`, `house_no`, `category_id`, `description`, `price`) VALUES
-(1, '623', 4, 'Sample', 2500);
+(1, '623', 4, 'Sample', 2500),
+(2, '34', 4, 'big big house', 10000),
+(3, '698', 4, 'big big one', 3566595),
+(4, '666', 2, 'bedsitter', 8600);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `maintenance_requests`
+--
+
+CREATE TABLE `maintenance_requests` (
+  `id` int(11) NOT NULL,
+  `request_text` text DEFAULT NULL,
+  `request_date` date DEFAULT NULL,
+  `house_no` varchar(50) DEFAULT NULL,
+  `username` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `maintenance_requests`
+--
+
+INSERT INTO `maintenance_requests` (`id`, `request_text`, `request_date`, `house_no`, `username`) VALUES
+(52, 'get nailed', '2024-03-26', '623', 'wambugu'),
+(53, 'nipige line', '2024-03-30', '69', 'mwaisilvester74@gmail.com'),
+(58, 'nataka mlango', '2024-03-28', '623', 'mwaisilvester74@gmail.com'),
+(59, 'thank you', '2024-03-22', '34', 'jwambugu@gmail.com'),
+(60, 'yyyy', '2024-04-06', '623', 'jwambugu@gmail.com');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `messages`
+--
+
+CREATE TABLE `messages` (
+  `message_id` int(11) NOT NULL,
+  `receiver_id` int(11) DEFAULT NULL,
+  `message` text DEFAULT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `messages`
+--
+
+INSERT INTO `messages` (`message_id`, `receiver_id`, `message`, `timestamp`) VALUES
+(42, 9, 'your rent is due wih 5 days', '2024-03-30 21:21:39'),
+(43, 12, 'we mzee', '2024-03-30 22:13:31'),
+(44, 13, 'lipa rent mzee', '2024-03-30 22:16:59'),
+(45, 13, 'you have remianng balance of 4500', '2024-03-31 07:49:37'),
+(46, 13, 'ghhh', '2024-03-31 14:51:51'),
+(47, 14, 'mm', '2024-04-02 15:26:26'),
+(48, 13, 'hhh', '2024-04-02 22:04:43');
 
 -- --------------------------------------------------------
 
@@ -75,15 +130,71 @@ CREATE TABLE `payments` (
   `amount` float NOT NULL,
   `invoice` varchar(50) NOT NULL,
   `date_created` datetime NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `payments`
 --
 
 INSERT INTO `payments` (`id`, `tenant_id`, `amount`, `invoice`, `date_created`) VALUES
-(1, 2, 2500, '123456', '2020-10-26 11:29:35'),
-(2, 2, 7500, '136654', '2020-10-26 11:30:21');
+(7, 4, 9090, '888', '2024-04-08 07:25:54');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `qrcode_payments`
+--
+
+CREATE TABLE `qrcode_payments` (
+  `qrcode` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reminders`
+--
+
+CREATE TABLE `reminders` (
+  `id` int(11) NOT NULL,
+  `due_date` date NOT NULL,
+  `frequency` varchar(20) NOT NULL,
+  `custom_frequency` varchar(50) DEFAULT NULL,
+  `notification_message` text NOT NULL,
+  `user_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `reminders`
+--
+
+INSERT INTO `reminders` (`id`, `due_date`, `frequency`, `custom_frequency`, `notification_message`, `user_id`) VALUES
+(1, '2024-03-30', 'monthly', NULL, 'rent is due', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `stripe_payments`
+--
+
+CREATE TABLE `stripe_payments` (
+  `id` int(11) NOT NULL,
+  `tenants_id` varchar(255) NOT NULL,
+  `amount` decimal(10,0) NOT NULL,
+  `charge_id` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `stripe_payments`
+--
+
+INSERT INTO `stripe_payments` (`id`, `tenants_id`, `amount`, `charge_id`, `created_at`) VALUES
+(17, '9', 788888, 'ch_3P0qUVFq0XdzIScy0Vtb6ZBt', '2024-04-01 19:26:00'),
+(18, '9', 77777, 'ch_3P11bdFq0XdzIScy0wh9DsDK', '2024-04-02 07:18:05'),
+(19, '9', 78888, 'ch_3P1DNmFq0XdzIScy0ftNYoSe', '2024-04-02 19:52:35'),
+(20, '15', 78888, 'ch_3P26OqFq0XdzIScy0XL3Sn91', '2024-04-05 06:37:21'),
+(21, '13', 899999, 'ch_3P30TcFq0XdzIScy0G64Hypg', '2024-04-07 18:30:00');
 
 -- --------------------------------------------------------
 
@@ -98,14 +209,14 @@ CREATE TABLE `system_settings` (
   `contact` varchar(20) NOT NULL,
   `cover_img` text NOT NULL,
   `about_content` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `system_settings`
 --
 
 INSERT INTO `system_settings` (`id`, `name`, `email`, `contact`, `cover_img`, `about_content`) VALUES
-(1, 'House Rental Management System', 'info@sample.comm', '+6948 8542 623', '1603344720_1602738120_pngtree-purple-hd-business-banner-image_5493.jpg', '&lt;p style=&quot;text-align: center; background: transparent; position: relative;&quot;&gt;&lt;span style=&quot;color: rgb(0, 0, 0); font-family: &amp;quot;Open Sans&amp;quot;, Arial, sans-serif; font-weight: 400; text-align: justify;&quot;&gt;&amp;nbsp;is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&rsquo;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.&lt;/span&gt;&lt;br&gt;&lt;/p&gt;&lt;p style=&quot;text-align: center; background: transparent; position: relative;&quot;&gt;&lt;br&gt;&lt;/p&gt;&lt;p style=&quot;text-align: center; background: transparent; position: relative;&quot;&gt;&lt;br&gt;&lt;/p&gt;&lt;p&gt;&lt;/p&gt;');
+(1, 'Rental Payment System', 'info@sample.comm', '+6948 8542 623', '1603344720_1602738120_pngtree-purple-hd-business-banner-image_5493.jpg', '&lt;p style=&quot;text-align: center; background: transparent; position: relative;&quot;&gt;&lt;span style=&quot;color: rgb(0, 0, 0); font-family: &amp;quot;Open Sans&amp;quot;, Arial, sans-serif; font-weight: 400; text-align: justify;&quot;&gt;&amp;nbsp;is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&rsquo;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.&lt;/span&gt;&lt;br&gt;&lt;/p&gt;&lt;p style=&quot;text-align: center; background: transparent; position: relative;&quot;&gt;&lt;br&gt;&lt;/p&gt;&lt;p style=&quot;text-align: center; background: transparent; position: relative;&quot;&gt;&lt;br&gt;&lt;/p&gt;&lt;p&gt;&lt;/p&gt;');
 
 -- --------------------------------------------------------
 
@@ -123,14 +234,41 @@ CREATE TABLE `tenants` (
   `house_id` int(30) NOT NULL,
   `status` tinyint(1) NOT NULL DEFAULT 1 COMMENT '1 = active, 0= inactive',
   `date_in` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `tenants`
 --
 
 INSERT INTO `tenants` (`id`, `firstname`, `middlename`, `lastname`, `email`, `contact`, `house_id`, `status`, `date_in`) VALUES
-(2, 'John', 'C', 'Smith', 'jsmith@sample.com', '+18456-5455-55', 1, 1, '2020-07-02');
+(3, 'mwas', '', 'silvesta', 'mwaisilvester74@gmail.com', '0720929929', 3, 0, '2024-03-23'),
+(4, 'silvesta', 'mwangi', 'mwai', 'mwaisilvester74@gmail.com', '0727679130', 3, 1, '2024-02-18'),
+(5, 'jay', 'konchizi', 'wambugu', 'jwambugu@gmail.com', '0701241956', 2, 1, '2024-03-08'),
+(6, 'mukiri', 'kitu', 'josphine', 'josphine@gmail.com', '0727679130', 4, 1, '2024-04-05');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tenants_accounts`
+--
+
+CREATE TABLE `tenants_accounts` (
+  `id` int(30) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tenants_accounts`
+--
+
+INSERT INTO `tenants_accounts` (`id`, `username`, `password`) VALUES
+(9, 'mwaisilvester74@gmail.com', '$2y$10$VZhkp991pxn3SdRVOPNDUuJH940kCG4164mi9rdzM8ZeGEV0BAIvK'),
+(12, 'joram', '$2y$10$9YB.3fkUao0Q9KtKFl1LHedFYr70I/qLwEQfFgCpvO1XkDPPlrfZO'),
+(13, 'jwambugu@gmail.com', '$2y$10$z6I7CWJMon3KvzfXroQ17.M1ojAoMvFivP4B9dSAG4zc36Wq3Rioi'),
+(14, 'wollan', '$2y$10$6wR0fZcGAR4gKvC9gVilROVmDtWsAK8sTRha2Smi3zULJI91MOHwW'),
+(15, 'silvergarvey@gmail.com', '$2y$10$O1oaT.IICAHfkbUaXhVZOOtLx9nYebnWCZBtlJ5V3eKpg.0Zpb5wC'),
+(16, 'gmail.com', '$2y$10$.zqNatLKBYUv1pyTQEOIIOKVm8udZYv3UCrVGv4oWF3ZQXVooNb76');
 
 -- --------------------------------------------------------
 
@@ -144,7 +282,7 @@ CREATE TABLE `users` (
   `username` varchar(200) NOT NULL,
   `password` text NOT NULL,
   `type` tinyint(1) NOT NULL DEFAULT 2 COMMENT '1=Admin,2=Staff'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `users`
@@ -153,15 +291,18 @@ CREATE TABLE `users` (
 INSERT INTO `users` (`id`, `name`, `username`, `password`, `type`) VALUES
 (1, 'Administrator', 'admin', '0192023a7bbd73250516f069df18b500', 1);
 
----- Table structure for table `maintenance_requests`
-CREATE TABLE `maintenance_requests` (
-    `request_id` INT AUTO_INCREMENT PRIMARY KEY,
-    `request_text` TEXT,
-    `request_date` DATE
-);
+-- --------------------------------------------------------
 
--- No need to include "Dumping data" section for this table as it will be empty initially
+--
+-- Table structure for table `user_profile`
+--
 
+CREATE TABLE `user_profile` (
+  `profile_id` int(30) NOT NULL,
+  `user_id` int(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
 -- Indexes for dumped tables
 --
 
@@ -178,9 +319,33 @@ ALTER TABLE `houses`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `maintenance_requests`
+--
+ALTER TABLE `maintenance_requests`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `messages`
+--
+ALTER TABLE `messages`
+  ADD PRIMARY KEY (`message_id`);
+
+--
 -- Indexes for table `payments`
 --
 ALTER TABLE `payments`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `reminders`
+--
+ALTER TABLE `reminders`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `stripe_payments`
+--
+ALTER TABLE `stripe_payments`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -196,10 +361,23 @@ ALTER TABLE `tenants`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `tenants_accounts`
+--
+ALTER TABLE `tenants_accounts`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user_profile`
+--
+ALTER TABLE `user_profile`
+  ADD PRIMARY KEY (`profile_id`),
+  ADD KEY `user_id` (`user_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -209,19 +387,43 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `houses`
 --
 ALTER TABLE `houses`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `maintenance_requests`
+--
+ALTER TABLE `maintenance_requests`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=61;
+
+--
+-- AUTO_INCREMENT for table `messages`
+--
+ALTER TABLE `messages`
+  MODIFY `message_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=49;
 
 --
 -- AUTO_INCREMENT for table `payments`
 --
 ALTER TABLE `payments`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `reminders`
+--
+ALTER TABLE `reminders`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `stripe_payments`
+--
+ALTER TABLE `stripe_payments`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- AUTO_INCREMENT for table `system_settings`
@@ -233,13 +435,35 @@ ALTER TABLE `system_settings`
 -- AUTO_INCREMENT for table `tenants`
 --
 ALTER TABLE `tenants`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `tenants_accounts`
+--
+ALTER TABLE `tenants_accounts`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
   MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `user_profile`
+--
+ALTER TABLE `user_profile`
+  MODIFY `profile_id` int(30) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `user_profile`
+--
+ALTER TABLE `user_profile`
+  ADD CONSTRAINT `user_profile_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `tenants_accounts` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
